9aecd45d1bc82b9531e4036cc88bfefae676ef94
usha <ushadevi.saitech@gmail.com>
2025-11-18 16:02:27 +0530

    partner io label changes

--- resources/views/pages/partner_io/view.blade.php
+++ resources/views/pages/partner_io/view.blade.php
@@ -51,7 +51,7 @@
                     <div style="display: flex; justify-content: space-between; align-items: flex-start; border-bottom: 1px solid #000;">
                         <div style="flex: 1; text-align: center; line-height: 1.6;">
                             <h5 style="font-size: 18px; font-weight: 600; letter-spacing: 1px; margin: 5px 0px 0px 0px;">
-                                {{ $partner_io_detail->outward_type=="RGP" ? 'Returnable Delivery Challan' : 'Non Returnable Delivery Challan' }}
+                                {{ $partner_io_detail->outward_type=="RGP" ? 'Returnable Gate Pass' : 'Non Returnable Gate Pass' }}
                             </h5>
                             <p style="font-size: 15px; margin-bottom: 10px; font-weight: 600;">
                                 (Rule 55 of CGST Rules 2017)
