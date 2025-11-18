11ceb37f128131123a4206b320ab182afb80d54b
usha <ushadevi.saitech@gmail.com>
2025-11-18 17:43:01 +0530

    print inspection

--- resources/views/pages/inspection/printInspectionReport.blade.php
+++ resources/views/pages/inspection/printInspectionReport.blade.php
@@ -90,114 +90,121 @@ $whiteLabelInfo = App\WhiteLabelSettings::first();
                     </div>
                 </div>
                 @php
-$columnsPerPage = $columnsPerPage ?? 13;
-$totalPages = $totalPages ?? 1;
-@endphp
+                    $actualQty = $manufacture->product_quantity;
+                    $columnsPerPage = 13; 
 
-@for ($page = 1; $page <= $totalPages; $page++)
-    @php
-        $startIndex = ($page - 1) * $columnsPerPage + 1;
-        $endIndex = min($startIndex + $columnsPerPage - 1, $manufacture->product_quantity);
-    @endphp
+                    if ($actualQty <= $columnsPerPage) {
+                        $totalPages = 1;
+                    } else {
+                        $totalPages = ceil($actualQty / $columnsPerPage);
+                    }
 
-    <div style="page-break-after: always; border:1px solid #000; border-top: none; margin-bottom: 20px;">
-        <table style="width:100%; border-collapse:collapse; font-size:16px;">
-            <tr style="text-align:center;">
-                <th style='border: 1px solid #000; border-left: none;'>Sl.No</th>
-                <th style='border: 1px solid #000; padding: 10px;'>PARAMETER</th>
-                <th style='border: 1px solid #000; padding: 10px;'>DRAWING <br> SPEC.</th>
-                <th style='border: 1px solid #000; padding: 10px;'>INSP. <br>METHOD</th>
-                <th style='border: 1px solid #000; border-right: none;' colspan="{{ $columnsPerPage }}">
-                    OBSERVED DIMENSIONS SL.No.
-                </th>
-            </tr>
+                @endphp
 
-            <tr>
-                <td style="border: 1px solid #000; padding: 5px; border-left: none;"></td>
-                <td style="border: 1px solid #000; padding: 5px;">&nbsp;</td>
-                <td style="border: 1px solid #000; padding: 5px;">&nbsp;</td>
-                <td style="border: 1px solid #000; padding: 5px;">&nbsp;</td>
-                @for ($i = $startIndex; $i <= $endIndex; $i++)
-                    <td style="border: 1px solid #000; padding: 5px; border-right:none;">
-                        DBF{{ $i }}
-                    </td>
+                @for ($page = 1; $page <= $totalPages; $page++)
+                    @php
+                        $startIndex = ($page - 1) * $columnsPerPage + 1;
+                        $endIndex = min($startIndex + $columnsPerPage - 1, max($actualQty, $columnsPerPage));
+                    @endphp
+
+                    <div style="page-break-after: always; border:1px solid #000; border-top: none; margin-bottom: 20px;">
+                        <table style="width:100%; border-collapse:collapse; font-size:16px;">
+                            <tr style="text-align:center;">
+                                <th style='border: 1px solid #000; border-left: none;'>Sl.No</th>
+                                <th style='border: 1px solid #000; padding: 10px;'>PARAMETER</th>
+                                <th style='border: 1px solid #000; padding: 10px;'>DRAWING <br> SPEC.</th>
+                                <th style='border: 1px solid #000; padding: 10px;'>INSP. <br>METHOD</th>
+                                <th style='border: 1px solid #000; border-right: none;' colspan="{{ $endIndex - $startIndex + 1 }}">
+                                    OBSERVED DIMENSIONS SL.No.
+                                </th>
+                            </tr>
+
+                            <tr>
+                                <td style="border: 1px solid #000; padding: 5px; border-left: none;"></td>
+                                <td style="border: 1px solid #000; padding: 5px;">&nbsp;</td>
+                                <td style="border: 1px solid #000; padding: 5px;">&nbsp;</td>
+                                <td style="border: 1px solid #000; padding: 5px;">&nbsp;</td>
+                                @for ($i = $startIndex; $i <= $endIndex; $i++)
+                                    <td style="border: 1px solid #000; padding: 5px; border-right:none;">
+                                        DBF{{ $i }}
+                                    </td>
+                                @endfor
+                            </tr>
+
+                            {{-- Dimension Inspection --}}
+                            @if(isset($inspection) && count($inspection) > 0)
+                                <tr>
+                                    <th></th>
+                                    <th colspan="{{ 4 + $columnsPerPage }}" style="text-align: start; padding: 10px 7px;">Dimension Inspection</th>
+                                </tr>
+                                @foreach($inspection as $key => $value)
+                                    @if($value->di_param!='')
+                                        <tr>
+                                            <td style="border: 1px solid #000; padding: 10px 7px; border-left: none;">{{ $loop->iteration }}</td>
+                                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->di_param }}</td>
+                                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->di_spec }}</td>
+                                            <td style="border: 1px solid #000; padding: 10px 7px; text-align:center;">{{ $value->di_method ?? '-' }}</td>
+
+                                            @php
+                                            $matchedDimensions = $di_inspect_dimensions->where('inspect_id', $value->id)->values();
+                                            @endphp
+                                            @for ($i = $startIndex; $i <= $endIndex; $i++)
+                                                @php $index = $i - 1; @endphp
+                                                <td style="border: 1px solid #000; border-right:none;">
+                                                    {{ $matchedDimensions[$index]->di_observed_dimension ?? '' }}
+                                                </td>
+                                            @endfor
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            @endif
+
+                            {{-- Appearance Inspection --}}
+                            @if(isset($inspection) && count($inspection) > 0)
+                                <tr>
+                                    <th></th>
+                                    <th colspan="{{ 4 + $columnsPerPage }}" style="text-align: start; padding: 10px 7px;">Appearance Inspection</th>
+                                </tr>
+                                @foreach($inspection as $key => $value)
+                                    @if($value->ap_param!='')
+                                        <tr>
+                                            <td style="border: 1px solid #000; padding: 10px 7px; border-left: none;">{{ $loop->iteration }}</td>
+                                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->ap_param }}</td>
+                                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->ap_spec }}</td>
+                                            <td style="border: 1px solid #000; padding: 10px 7px; text-align:center;">{{ $value->ap_method ?? '-' }}</td>
+
+                                            @php
+                                            $matchedApDimensions = $ap_inspect_dimensions->where('inspect_id', $value->id)->values();
+                                            @endphp
+                                            @for ($i = $startIndex; $i <= $endIndex; $i++)
+                                                @php $index = $i - 1; @endphp
+                                                <td style="border: 1px solid #000; border-right:none;">
+                                                    {{ $matchedApDimensions[$index]->ap_observed_dimension ?? '' }}
+                                                </td>
+                                            @endfor
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            @endif
+                        </table>
+
+                        {{-- Footer --}}
+                        <div style="display: flex; justify-content: space-around; margin-top: 20px;">
+                            <div>
+                                <p>INSPECTED BY</p>
+                                <h5>{{ !empty($inspection_approval) && is_object($inspection_approval) ? getEmpCode($inspection_approval->inspected_by) : '' }}</h5>
+                            </div>
+                            <div>
+                                <p>CHECKED BY</p>
+                                <h5>{{ !empty($inspection_approval) && is_object($inspection_approval) ? getEmpCode($inspection_approval->checked_by) : '' }}</h5>
+                            </div>
+                        </div>
+
+                        <div style="text-align:center; margin-top:10px;">
+                            <strong>Page {{ $page }} of {{ $totalPages }}</strong>
+                        </div>
+                    </div>
                 @endfor
-            </tr>
-
-            {{-- Dimension Inspection --}}
-            @if(isset($inspection) && count($inspection) > 0)
-                <tr>
-                    <th></th>
-                    <th colspan="{{ 4 + $columnsPerPage }}" style="text-align: start; padding: 10px 7px;">Dimension Inspection</th>
-                </tr>
-                @foreach($inspection as $key => $value)
-                    @if($value->di_param!='')
-                        <tr>
-                            <td style="border: 1px solid #000; padding: 10px 7px; border-left: none;">{{ $loop->iteration }}</td>
-                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->di_param }}</td>
-                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->di_spec }}</td>
-                            <td style="border: 1px solid #000; padding: 10px 7px; text-align:center;">{{ $value->di_method ?? '-' }}</td>
-
-                            @php
-                            $matchedDimensions = $di_inspect_dimensions->where('inspect_id', $value->id)->values();
-                            @endphp
-                            @for ($i = $startIndex; $i <= $endIndex; $i++)
-                                @php $index = $i - 1; @endphp
-                                <td style="border: 1px solid #000; border-right:none;">
-                                    {{ $matchedDimensions[$index]->di_observed_dimension ?? '' }}
-                                </td>
-                            @endfor
-                        </tr>
-                    @endif
-                @endforeach
-            @endif
-
-            {{-- Appearance Inspection --}}
-            @if(isset($inspection) && count($inspection) > 0)
-                <tr>
-                    <th></th>
-                    <th colspan="{{ 4 + $columnsPerPage }}" style="text-align: start; padding: 10px 7px;">Appearance Inspection</th>
-                </tr>
-                @foreach($inspection as $key => $value)
-                    @if($value->ap_param!='')
-                        <tr>
-                            <td style="border: 1px solid #000; padding: 10px 7px; border-left: none;">{{ $loop->iteration }}</td>
-                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->ap_param }}</td>
-                            <td style="border: 1px solid #000; padding: 10px 7px;">{{ $value->ap_spec }}</td>
-                            <td style="border: 1px solid #000; padding: 10px 7px; text-align:center;">{{ $value->ap_method ?? '-' }}</td>
-
-                            @php
-                            $matchedApDimensions = $ap_inspect_dimensions->where('inspect_id', $value->id)->values();
-                            @endphp
-                            @for ($i = $startIndex; $i <= $endIndex; $i++)
-                                @php $index = $i - 1; @endphp
-                                <td style="border: 1px solid #000; border-right:none;">
-                                    {{ $matchedApDimensions[$index]->ap_observed_dimension ?? '' }}
-                                </td>
-                            @endfor
-                        </tr>
-                    @endif
-                @endforeach
-            @endif
-        </table>
-
-        {{-- Footer --}}
-        <div style="display: flex; justify-content: space-around; margin-top: 20px;">
-            <div>
-                <p>INSPECTED BY</p>
-                <h5>{{ !empty($inspection_approval) && is_object($inspection_approval) ? getEmpCode($inspection_approval->inspected_by) : '' }}</h5>
-            </div>
-            <div>
-                <p>CHECKED BY</p>
-                <h5>{{ !empty($inspection_approval) && is_object($inspection_approval) ? getEmpCode($inspection_approval->checked_by) : '' }}</h5>
-            </div>
-        </div>
-
-        <div style="text-align:center; margin-top:10px;">
-            <strong>Page {{ $page }} of {{ $totalPages }}</strong>
-        </div>
-    </div>
-@endfor
                 <p style="padding-top: 5px; padding-bottom: 100px; font-size: 13px;"><b>Remarks</b>&nbsp;&nbsp;{{ !empty($inspection_approval) && is_object($inspection_approval) ? $inspection_approval->remarks : '' }}</p>
             </div>
         </div>
